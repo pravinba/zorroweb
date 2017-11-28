7dda735afae4ace7fe20ff813dddb4f4f0ee9651
pravinba <pravinu007@msn.com>
2017-11-28 18:00:13 +0530

    Prod config file upload

--- src/main/java/com/zorro/config/ProductionConfig.java
+++ src/main/java/com/zorro/config/ProductionConfig.java
@@ -1,12 +1,20 @@
 package com.zorro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
+import com.zorro.backend.service.EmailService;
+import com.zorro.backend.service.smtpEmailService;
+
 @Configuration
 @Profile("prod")
-@PropertySource("/resources/application-prod.properties")
+@PropertySource("application-prod.properties")
 public class ProductionConfig {
-
+	
+	@Bean
+    public EmailService emailService() {
+        return new smtpEmailService();
+    }
 }
