4dc39bae16eee5b68c466dc8074891b41532456e
pravinba <pravinu007@msn.com>
2017-11-29 11:50:10 +0530

    Updated H2 DB changes and JPA

---
+++ src/main/java/com/zorro/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,50 @@
+package com.zorro.backend.persistence.domain.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private int id;	
+	private String name;
+	
+	public Plan() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Plan plan = (Plan) o;
+
+        return id == plan.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
