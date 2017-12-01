b310c44727e87efa0e9b4480140d3830e58d35c1
pravinba <pravinu007@msn.com>
2017-12-01 14:40:46 +0530

    Expecting break 2 - Integration Test for service Layer

---
+++ src/main/java/com/zorro/utils/UsersUtil.java
@@ -0,0 +1,28 @@
+package com.zorro.utils;
+
+import com.zorro.backend.persistence.domain.backend.User;
+
+public class UsersUtil {
+	
+	private UsersUtil () {
+		throw new AssertionError("Non instantiable !");
+	}
+	
+	public static User createBasicUser() {
+		
+		User user = new User();
+		user.setUsername("basicUser");
+		user.setFirstName("firstName");
+		user.setLastName("lastName");
+		user.setPassword("secret");
+		user.setEmail("me@test.com");
+		user.setPhoneNumber("1234567890");
+		user.setCountry("GB");
+		user.setDescription("A basic user");		
+		user.setEnabled(true);
+		user.setProfileImageUrl("http://bla@bla.com");
+		
+		return user;
+	}
+
+}
---
+++ src/test/java/com/zorro/test/integration/UserServiceIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.zorro.test.integration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import com.zorro.ZorrowebApplication;
+import com.zorro.backend.persistence.domain.backend.Plan;
+import com.zorro.backend.persistence.domain.backend.Role;
+import com.zorro.backend.persistence.domain.backend.User;
+import com.zorro.backend.persistence.domain.backend.UserRole;
+import com.zorro.backend.service.UserService;
+import com.zorro.enums.PlansEnum;
+import com.zorro.enums.RolesEnum;
+import com.zorro.utils.UsersUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ZorrowebApplication.class)
+public class UserServiceIntegrationTest {
+	@Autowired
+	UserService userService;
+	
+	@Test
+	public void testCreateNewUser() throws Exception{
+		
+		User basicUser = UsersUtil.createBasicUser();
+		
+		Set<UserRole> userRoles = new HashSet<>();
+		userRoles.add(new UserRole(basicUser, new Role(RolesEnum.BASIC)));
+		
+				
+		User newUser = userService.CreateUser(basicUser, PlansEnum.BASIC, userRoles);
+		
+		Assert.notNull(newUser);
+		Assert.notNull(newUser.getId());
+		
+	}
+
+}
