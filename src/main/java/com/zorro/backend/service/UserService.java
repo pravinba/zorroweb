0d78fe8baef0588c2894b0f73ffd2048614e6dac
pravinba <pravinu007@msn.com>
2017-12-01 12:39:04 +0530

    Expecting break 1 - Define Service Layer

---
+++ src/main/java/com/zorro/backend/service/UserService.java
@@ -0,0 +1,52 @@
+package com.zorro.backend.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zorro.backend.persistence.domain.backend.Plan;
+import com.zorro.backend.persistence.domain.backend.User;
+import com.zorro.backend.persistence.domain.backend.UserRole;
+import com.zorro.backend.persistence.repositories.PlanRepository;
+import com.zorro.backend.persistence.repositories.RoleRepository;
+import com.zorro.backend.persistence.repositories.UserRepository;
+import com.zorro.enums.PlansEnum;
+
+@Service
+@Transactional(readOnly=true)
+public class UserService {
+	@Autowired
+	private PlanRepository planRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	public User CreateUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
+		
+		/* Create a plan & persist to DB */
+		
+		Plan plan = new Plan(plansEnum);
+		if(!planRepository.exists(plansEnum.getId()))
+		{
+			plan = planRepository.save(plan);
+		}		
+			
+		/* Set Role & Plan to User Object */
+		user.setPlan(plan);
+		
+		for(UserRole usr : userRoles) {
+			roleRepository.save(usr.getRole());
+		}		
+		user.getUserRoles().addAll(userRoles);
+		
+		user = userRepository.save(user);
+		
+		return user;
+	}
+
+}
--- src/main/java/com/zorro/config/ApplicationConfig.java
+++ src/main/java/com/zorro/config/ApplicationConfig.java
@@ -2,10 +2,11 @@ package com.zorro.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaRepositories(basePackages="com.zorro.backend.persistence.repositories")
 @EntityScan(basePackages="com.zorro.backend.persistence.domain.backend")
-
+@EnableTransactionManagement
 public class ApplicationConfig {
 
 }
